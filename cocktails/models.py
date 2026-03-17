9c0ad5ff43e10784e70dc11e13e39e29fb02041f
Oliwier K <o.krawczyk06@gmail.com>
2026-03-17 17:32:40 +0100

    feat:added the basic Ingredient and Cocktail models

--- cocktails/models.py
+++ cocktails/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Cocktail(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    category = models.CharField(max_length=100)
+    instructions = models.TextField()
+
+    #manytomany through table 
+    ingredients = models.ManyToManyField(Ingredient, through='CocktailIngredient')
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True,null=True)
+    is_alcoholic = models.BooleanField(default=True)
+    image = models.ImageField(upload_to="ingredients/",blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CocktailIngredient(models.Model):
+    cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.amount} of {self.ingredient.__str__(self.ingredient)} in {self.cocktail.__str__(self.cocktail)}"
