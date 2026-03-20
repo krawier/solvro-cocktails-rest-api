2b6d58eeaec872b280bbc34a5ffc5672c36e5808
Oliwier K <o.krawczyk06@gmail.com>
2026-03-20 21:27:55 +0100

    feat: add serializers and migrate

--- cocktails/models.py
+++ cocktails/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True,null=True)
+    is_alcoholic = models.BooleanField(default=True)
+    image = models.ImageField(upload_to="ingredients/",blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Cocktail(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -13,20 +20,10 @@ class Cocktail(models.Model):
         return self.name
 
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True,null=True)
-    is_alcoholic = models.BooleanField(default=True)
-    image = models.ImageField(upload_to="ingredients/",blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
 class CocktailIngredient(models.Model):
     cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     amount = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.amount} of {self.ingredient.__str__(self.ingredient)} in {self.cocktail.__str__(self.cocktail)}"
+        return f"{self.amount} of {self.ingredient.name} in {self.cocktail.name}"
---
+++ cocktails/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Cocktail,Ingredient,CocktailIngredient
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id','name','description',"is_alcoholic","image"]
+
+class CocktailIngredientSerializer(serializers.ModelSerializer):
+    # added so we see more than just the id 
+    ingredient_name = serializers.ReadOnlyField(source='ingredient.name')
+
+    class Meta:
+        model = CocktailIngredient
+        fields = ['ingredient','ingredient_name','amount']
+
+class CocktailSerialize(serializers.ModelSerializer):
+    # we want to see the details
+    ingredient_detail = CocktailIngredientSerializer(source='cocktailingredient_set',many=True,read_only=True)
+
+    class Meta:
+        model = Cocktail
+        fields = ['id','name','category','instructions','ingredients_detail']              
