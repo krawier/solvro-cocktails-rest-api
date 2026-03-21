a52c5b839ad5f22d432ede29bdd31d5c956cf3c9
Oliwier K <o.krawczyk06@gmail.com>
2026-03-21 21:30:44 +0100

    fix: add favorites url

--- cocktails/urls.py
+++ cocktails/urls.py
@@ -5,7 +5,8 @@ urlpatterns =[
 
     path('cocktails/', views.CocktailListCreateView.as_view(), name='cocktail-list'),
     path('cocktails/<int:pk>/', views.CocktailRetrieveUpdateDestroyView.as_view(), name='cocktail-detail'),
-
+    path('cocktails/<int:pk>/favorite/', views.CocktailFavoriteToggleView.as_view(), name='cocktail-favorite-toggle'),
+    path('favorites/', views.FavoriteCocktailListView.as_view(), name='favorite-list'),
     path('ingredients/', views.IngredientListCreateView.as_view(), name='ingredient-list'),
     path('ingredients/<int:pk>/', views.IngredientRetrieveUpdateDestroyView.as_view(), name='ingredient-detail'),
 
