01c1fb71976c5e56653b35fea87722be8dd07fb7
Oliwier K <o.krawczyk06@gmail.com>
2026-03-21 21:56:17 +0100

    feat: add docker

--- core/settings.py
+++ core/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'solvro_cocktails',
+        'USER': 'solvro_admin',
+        'PASSWORD': 'supertajnehaslo123',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
  
