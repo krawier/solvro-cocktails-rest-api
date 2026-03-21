afc31304b2bf131aac3ff0d24d453c899ddc0a21
Oliwier K <o.krawczyk06@gmail.com>
2026-03-21 22:35:48 +0100

    fix: change password in settings to match docker password

--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'solvro_cocktails',
         'USER': 'solvro_admin',
-        'PASSWORD': 'supertajnehaslo123',
+        'PASSWORD': 'P@ssw0rd',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
