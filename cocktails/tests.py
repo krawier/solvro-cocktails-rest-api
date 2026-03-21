1ba62368746da9f6452bfafd8267fde66a22deac
Oliwier K <o.krawczyk06@gmail.com>
2026-03-21 11:57:50 +0100

    feat: add tests

--- cocktails/tests.py
+++ cocktails/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Cocktail,Ingredient
 
-# Create your tests here.
+class CocktailApiTests(APITestCase):
+
+    def test_get_cocktails_list_returns_200(self):
+        url = '/api/cocktails/'
+        response =self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_unauthenticated_user_cannot_create_ingredient(self):
+
+        url = '/api/ingredients/'
+        data = {'name: Wódka','is_alcoholic:True'} 
+        response = self.client.post(url,data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)   
